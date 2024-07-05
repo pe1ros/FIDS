a7091619011da3b870a3d51455e5c42d9d398e18
Ilia Petrosian <im.clean@yandex.ru>
2024-07-05 10:10:27 +0300

    Change UI , add toggle search bar, that may be hidden

--- App.tsx
+++ App.tsx
@@ -1,118 +1,134 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
+  TextInput,
   View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
 } from 'react-native';
+import WebView from 'react-native-webview';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): JSX.Element {
+  const [width, setWidth] = useState(375);
+  const [height, setHeight] = useState(812);
+  const [url, setUrl] = useState('http://10.96.70.204:3000/?id=4');
+  const [isVisible, setIsVisible] = useState(true);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleChangeVisible = () => {
+    setIsVisible(!isVisible);
+  };
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handleChangeUrl = (val: string) => {
+    setUrl(val);
+  };
+
+  const styleWithKey = (
+    key: keyof ReturnType<typeof styles>,
+    isBox?: boolean,
+  ) => {
+    return [
+      styles(width)[key],
+      isBox && styles(width).styleBorderBox,
+      !isVisible && styles(width).transparent,
+    ];
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View
+      style={styleWithKey('container')}
+      onLayout={({nativeEvent}) => {
+        setWidth(nativeEvent.layout.width);
+        setHeight(nativeEvent.layout.height);
+      }}>
+      <View style={styleWithKey('navBar')}>
+        <TextInput
+          style={styleWithKey('input')}
+          value={url}
+          onChangeText={handleChangeUrl}
+        />
+        <View style={styleWithKey('sizeBlock', true)}>
+          <Text style={styleWithKey('sizeText')}>w: {Math.round(width)}</Text>
+          <Text style={styleWithKey('sizeText')}>h: {Math.round(height)}</Text>
+        </View>
+        <TouchableOpacity
+          style={styleWithKey('sizeBlock', true)}
+          onPress={handleChangeVisible}>
+          {isVisible && (
+            <ImageBackground
+              source={require('./ArrowTop.png')}
+              style={styleWithKey('image')}
+            />
+          )}
+        </TouchableOpacity>
+      </View>
+      <WebView style={styleWithKey('webView')} source={{uri: url}} />
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = (width: number) =>
+  StyleSheet.create({
+    container: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'white',
+    },
+    webView: {
+      width: '100%',
+      height: '100%',
+    },
+    navBar: {
+      flexDirection: 'row',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      zIndex: 999,
+      backgroundColor: 'white',
+      height: 40,
+    },
+    input: {
+      height: 40,
+      color: '#060708',
+      width: width - 110,
+      borderWidth: 1,
+      borderColor: '#060708',
+    },
+    sizeBlock: {
+      width: 70,
+      height: 40,
+      justifyContent: 'center',
+    },
+    sizeText: {
+      lineHeight: 15,
+      fontSize: 12,
+      alignSelf: 'center',
+      color: '#060708',
+    },
+    transparent: {
+      color: 'transparent',
+      backgroundColor: 'transparent',
+      borderColor: 'transparent',
+    },
+    button: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      maxWidth: 40,
+      maxHeight: 40,
+    },
+    image: {
+      width: 36,
+      height: 36,
+      resizeMode: 'contain',
+    },
+    styleBorderBox: {
+      borderTopWidth: 1,
+      borderTopColor: '#060708',
+      borderRightWidth: 1,
+      borderRightColor: '#060708',
+      borderBottomWidth: 1,
+      borderBottomColor: '#060708',
+    },
+  });
 
 export default App;
